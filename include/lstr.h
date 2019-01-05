feb0c0b0f7c2b7fbc5efa2060306d118d96df698
Leandre <leandre.blanchard@epitech.eu>
2019-01-05 17:29:34 +0100

    use of sys/cdefs.h for __THROW and __nonnull macros

--- include/lstr.h
+++ include/lstr.h
@@ -9,6 +9,7 @@
 # define _LBLSTR_H
 
 # include <sys/types.h>
+# include <sys/cdefs.h>
 
 typedef struct lblstr_s {
 	char *i;
@@ -17,29 +18,29 @@ typedef struct lblstr_s {
 	pthread_mutex_t lock;
 } lstr_t;
 
-int lstr_create(lstr_t *lstr_addr, const char *str);
-ssize_t lstr_fd(lstr_t *lstr_addr, int fd, ssize_t count);
-ssize_t lstr_file(lstr_t *lstr_addr, const char *pathname, ssize_t count);
-int lstr_format(lstr_t *lstr, const char *format, ...);
+int lstr_create(lstr_t *lstr_addr, const char *str) __THROW __nonnull((1));
+ssize_t lstr_fd(lstr_t *lstr_addr, int fd, ssize_t count) __THROW __nonnull((1));
+ssize_t lstr_file(lstr_t *lstr_addr, const char *pathname, ssize_t count) __THROW __nonnull((1, 2));
+int lstr_format(lstr_t *lstr, const char *format, ...) __THROW __nonnull((1, 2)) __attribute__ ((__format__ (__printf__, 2, 0)));
 
-void lstr_destroy(lstr_t *lstr);
+void lstr_destroy(lstr_t *lstr) __THROW __nonnull((1));
 
-int lstr_append(lstr_t *lstr, const char *str);
-int lstr_concat(lstr_t *lstr, const lstr_t *add);
-int lstr_set(lstr_t *lstr, const char *src, size_t len);
-int lstr_resize(lstr_t *lstr, size_t new_size);
-int lstr_addch(lstr_t *lstr, char c);
-int lstr_dup(lstr_t *dest, const lstr_t *lstr);
-void lstr_shift(lstr_t *lstr, int offset);
-void lstr_remove(lstr_t *lstr, const char *pattern);
-int lstr_replace(lstr_t *lstr, const char *replace, const char *by);
-int lstr_shrink_to_fit(lstr_t *lstr);
-int lstr_insert(lstr_t *lstr, size_t pos, const char *str);
-void lstr_clear(lstr_t *lstr);
-void lstr_erase(lstr_t *lstr, size_t pos, size_t len);
-void lstr_swap(lstr_t *l1, lstr_t *l2);
+int lstr_append(lstr_t *lstr, const char *str) __THROW __nonnull((1, 2));
+int lstr_concat(lstr_t *lstr, const lstr_t *add) __THROW __nonnull((1, 2));
+int lstr_set(lstr_t *lstr, const char *src, size_t len) __THROW __nonnull((1, 2));
+int lstr_resize(lstr_t *lstr, size_t new_size) __THROW __nonnull((1));
+int lstr_addch(lstr_t *lstr, char c) __THROW __nonnull((1));
+int lstr_dup(lstr_t *dest, const lstr_t *lstr) __THROW __nonnull((1, 2));
+void lstr_shift(lstr_t *lstr, int offset) __THROW __nonnull((1));
+void lstr_remove(lstr_t *lstr, const char *pattern) __THROW __nonnull((1, 2));
+int lstr_replace(lstr_t *lstr, const char *replace, const char *by) __THROW __nonnull((1, 2, 3));
+int lstr_shrink_to_fit(lstr_t *lstr) __THROW __nonnull((1));
+int lstr_insert(lstr_t *lstr, size_t pos, const char *str) __THROW __nonnull((1, 3));
+void lstr_clear(lstr_t *lstr) __THROW __nonnull((1));
+void lstr_erase(lstr_t *lstr, size_t pos, size_t len) __THROW __nonnull((1));
+void lstr_swap(lstr_t *l1, lstr_t *l2) __THROW __nonnull((1, 2));
 
-int lstr_lock(lstr_t *lstr);
-int lstr_unlock(lstr_t *lstr);
+int lstr_lock(lstr_t *lstr) __THROW __nonnull((1));
+int lstr_unlock(lstr_t *lstr) __THROW __nonnull((1));
 
 #endif /* !_LBLSTR_H */
